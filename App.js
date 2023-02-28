2804d5499b2a9c254688f0efd74c4f6aa28b39a6
MoOsman5 <mohamed1592000gmail.com>
2023-02-28 17:29:13 +0200

    create AwesomeProject

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#900a0a',
     alignItems: 'center',
     justifyContent: 'center',
   },
